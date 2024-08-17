eac61164e17ab8b8436a5439221088f1bae85926
Dass33 <94531241+Dass33@users.noreply.github.com>
2024-08-17 23:01:59 +0200

    Revert "Revert "degustacni menu""
    This reverts commit 87b1ad159c723ec1518ce436a801e5025ad84efc.

--- dist/reservation.js
+++ dist/reservation.js
@@ -1,8 +1,11 @@
 "use strict";
+const headerDiv = document.getElementById("header");
 const reservationPopup = document.getElementById("reservationPopUp");
 const reservationCloseButton = document.getElementById("closeReservationMenu");
-const headerDiv = document.getElementById("header");
 const reservationButtons = Array.from(document.getElementsByClassName("reservation"));
+const reservationPopupEvents = document.getElementById("reservationPopUpEvents");
+const reservationCloseButtonEvents = document.getElementById("closeReservationMenuEvents");
+const reservationButtonsEvents = Array.from(document.getElementsByClassName("reservationEvents"));
 if (reservationPopup) {
     reservationButtons.forEach((element) => {
         element.addEventListener("click", () => {
@@ -20,5 +23,20 @@ if (reservationCloseButton && reservationPopup) {
         document.body.style.overflowY = "scroll";
         // if (headerDiv) headerDiv.style.display = "block";
         // if (mapIcon) mapIcon.style.display = "block";
+    });
+}
+if (reservationPopupEvents) {
+    reservationButtonsEvents.forEach((element) => {
+        element.addEventListener("click", () => {
+            reservationPopupEvents.style.display = "block";
+            document.body.style.overflowY = "hidden";
+            closeNav();
+        });
+    });
+}
+if (reservationCloseButtonEvents && reservationPopupEvents) {
+    reservationCloseButtonEvents.addEventListener("click", () => {
+        reservationPopupEvents.style.display = "none";
+        document.body.style.overflowY = "scroll";
     });
 }
--- src/reservation.ts
+++ src/reservation.ts
@@ -1,8 +1,14 @@
-const reservationPopup = document.getElementById("reservationPopUp");
-const reservationCloseButton = document.getElementById("closeReservationMenu");
 const headerDiv = document.getElementById("header");
 
+const reservationPopup = document.getElementById("reservationPopUp");
+const reservationCloseButton = document.getElementById("closeReservationMenu");
+
 const reservationButtons = Array.from(document.getElementsByClassName("reservation"));
+
+const reservationPopupEvents = document.getElementById("reservationPopUpEvents");
+const reservationCloseButtonEvents = document.getElementById("closeReservationMenuEvents");
+
+const reservationButtonsEvents = Array.from(document.getElementsByClassName("reservationEvents"));
 
 if (reservationPopup) {
 	reservationButtons.forEach((element) => {
@@ -25,3 +31,19 @@ if (reservationCloseButton && reservationPopup) {
 	});
 }
 
+if (reservationPopupEvents) {
+	reservationButtonsEvents.forEach((element) => {
+		element.addEventListener("click", () => {
+			reservationPopupEvents.style.display = "block";
+			document.body.style.overflowY = "hidden"
+			closeNav();
+		});
+	});
+}
+
+if (reservationCloseButtonEvents && reservationPopupEvents) {
+	reservationCloseButtonEvents.addEventListener("click", () => {
+		reservationPopupEvents.style.display = "none";
+		document.body.style.overflowY = "scroll"
+	});
+}
